3f609e9748d6c4aec6b3ae5b1a5c4538dd45a637
Donghyuk <mrgamza@gmail.com>
2023-06-17 00:04:51 +0900

    fix jest error

--- test/controller/RootController.test.js
+++ test/controller/RootController.test.js
@@ -9,7 +9,7 @@ const mockRequest = (sessionData) => {
 const mockResponse = () => {
    const res = {}
    res.status = jest.fn().mockReturnValue(res)
-   res.json = jest.fn().mockReturnValue(res)
+   res.end = jest.fn().mockReturnValue(res)
    return res;
 }
 
